744647c47530925e15514408797b4f582704c808
ShardulB <58384725+shardul-b@users.noreply.github.com>
2023-06-04 00:20:37 +0530

    Create login_script.js

---
+++ Scripts/login_script.js
@@ -0,0 +1,22 @@
+// Login Submit button clicked
+elementSelector('#login-submit').addEventListener('click', () => {
+  // This function automatically generates your object based on id and value just make sure you are using the right ids (based on your api)
+  let detailsObj = {};
+  let userInputRef = elementSelector('.login-input', true);
+  userInputRef.forEach((element) => {
+    detailsObj[element.id] = element.value;
+  });
+  console.log(detailsObj);
+  //Add your URL
+  fetch('url', {
+    Method: 'POST',
+    Body: detailsObj,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //data
+    })
+    .catch((error) => {
+      //handle error
+    });
+});
